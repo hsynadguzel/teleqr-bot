63f869b70eb27184fc2eaf967cc4436c4b4b0ef2
hsynadguzel <huseyin.00.03.09@gmail.com>
2024-05-15 10:29:05 +0300

    Added functionality to generate QR codes from user input and send them as photos.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import qr from 'qrcode';
 import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Replace the value below with the Telegram token you receive from @BotFather
-const token = '<YOUR_TELEGRAM_BOT_TOKEN>';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
